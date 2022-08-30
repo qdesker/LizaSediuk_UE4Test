e6891232cf7c98c8f52251aea61d7694a963a63e
Qdesker <69822581+qdesker@users.noreply.github.com>
2022-08-30 13:32:39 +0300

    Second task complited!

---
+++ Source/UI/Widgets/InGame/HUD/InGameHUDHealthBar.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InGameHUDHealthBar.h"
+
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+#include "Gameplay/Characters/PlayerCharacter.h"
+#include "Tools/Toolbox.h"
+#include "Gameplay/Components/Common/CharacterHealthComponent.h"
+
+void UInGameHUDHealthBar::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	const UWorld* pWorld = GetWorld();
+	if (!pWorld)
+	{
+		return;
+	}
+
+	const APlayerCharacter* pPlayer = UToolbox::GetCurrentPlayerCharacter(pWorld);
+	if (!pPlayer)
+	{
+		return;
+	}
+
+	auto* pHealthComponent = Cast<UCharacterHealthComponent>(pPlayer->GetComponentByClass(UCharacterHealthComponent::StaticClass()));
+	if(!IsValid(pHealthComponent))
+	{
+		return;
+	}
+	HealthComponentWeakPtr = TWeakObjectPtr<UCharacterHealthComponent>(pHealthComponent);
+
+	pHealthComponent->OnHealthChanged.AddDynamic(this, &UInGameHUDHealthBar::OnHealthChanged);
+
+	ChangeState(pHealthComponent->GetInitialHealthValue(), pHealthComponent->GetInitialHealthValue());
+}
+
+void UInGameHUDHealthBar::OnHealthChanged()
+{
+	if (!HealthComponentWeakPtr.IsValid())
+	{
+		return;
+	}
+
+	const UCharacterHealthComponent* pHealthComponent = HealthComponentWeakPtr.Get();
+
+	ChangeState(pHealthComponent->GetCurrentHealthValue(), pHealthComponent->GetInitialHealthValue());
+
+}
+
+void UInGameHUDHealthBar::ChangeState(float CurrentHealth, float InitialHealth)
+{
+	Health->SetPercent(CurrentHealth / InitialHealth);
+
+	HealthValue->SetText(FText::FromString(FString::FromInt(CurrentHealth) + "/" + FString::FromInt(InitialHealth)));
+}
---
+++ Source/UI/Widgets/InGame/HUD/InGameHUDHealthBar.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/DefaultUserWidget.h"
+#include "UObject/Object.h"
+#include "InGameHUDHealthBar.generated.h"
+
+class UCharacterHealthComponent;
+class UTextBlock;
+class UProgressBar;
+/**
+ *
+ */
+UCLASS()
+class CASTLEDEFENDER_API UInGameHUDHealthBar : public UDefaultUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeConstruct() override;
+
+
+private:
+	UFUNCTION()
+	void OnHealthChanged();
+
+	void ChangeState(float CurrentHealth, float InitialHealth);
+
+	UPROPERTY(meta = (BindWidget))
+	UProgressBar* Health;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* HealthValue;
+
+	TWeakObjectPtr<UCharacterHealthComponent> HealthComponentWeakPtr;
+};
